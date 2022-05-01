7457df7b3c13b6a0a66120d979164abe07253ada
NickParasiris <90874179+NickParasiris@users.noreply.github.com>
2022-04-30 22:54:27 -0400

    Removing mistake

--- Assignment_1_Nicholas_Parasiris/Q6.php
+++ Assignment_1_Nicholas_Parasiris/Q6.php
@@ -1,8 +1,5 @@
 <?php
 
-//Taken From the wikipidia article on the Boeing 737
-$text = "The Boeing 737 Next Generation, commonly abbreviated as 737NG, or 737 Next Gen, is a narrow-body aircraft powered by two jet engines and produced by Boeing Commercial Airplanes. Launched in 1993 as the third generation derivative of the Boeing 737, it has been produced since 1997 and is an upgrade of the 737 Classic series."
-
 
 
 
